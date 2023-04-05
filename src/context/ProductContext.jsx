784efd7cb0f91666c125c06378618b1f70e68d29
AminMusah <ameenmusah77@gmail.com>
2023-04-05 11:31:52 -0500

    update check box state

--- src/context/ProductContext.jsx
+++ src/context/ProductContext.jsx
@@ -136,7 +136,7 @@ const ProductProvider = ({ children }) => {
           console.error(error);
         });
 
-        setCheck(true)
+        setCheck(!check)
       window.location.replace("/");
     } catch (error) {
       console.log(error);
