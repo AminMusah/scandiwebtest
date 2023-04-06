6e0bc3ddf6ab59da203fab2b0c1538351090af6e
AminMusah <ameenmusah77@gmail.com>
2023-04-06 16:20:39 -0500

    remove delete checkbox

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -24,10 +24,9 @@ function Header() {
                 ADD
               </Link>{" "}
               <button
-                className={check ? "btn secondary-btn" : "btn secondary-btn delete-checkbox"}
+                className="btn secondary-btn"
                 onClick={() => {
                   handleDelete(id);
-                  setCheck(!check)
                 }}
               >
                 MASS DELETE
