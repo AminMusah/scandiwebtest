f1e0a98187b4fd5c14a9a86ab51f010177331b2f
AminMusah <ameenmusah77@gmail.com>
2023-04-06 19:07:01 -0500

    update ceheck state

--- src/context/ProductContext.jsx
+++ src/context/ProductContext.jsx
@@ -20,7 +20,7 @@ const ProductProvider = ({ children }) => {
   const [skuMessage, setSkuMessage] = useState(false);
   const [validate, setValidate] = useState(false);
   const [id, setId] = useState([]);
-  const [check, setCheck] = useState(false);
+  const [check, setCheck] = useState(true);
 
 
   //Get All products
